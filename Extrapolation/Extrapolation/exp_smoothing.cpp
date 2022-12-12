bb1e9512811d4b3e16a60c248a4424e0ac2ffa04
UstanovkaShkafov <Sky.spawta@gmail.com>
2022-12-12 23:01:33 +0300

    smoothing(min changes)

--- Extrapolation/Extrapolation/exp_smoothing.cpp
+++ Extrapolation/Extrapolation/exp_smoothing.cpp
@@ -9,20 +9,18 @@ std::vector<double> exponential_smoothing(std::vector<double> x, std::vector<dou
 	{
 		double predict_value = y[y.size() - 1];
 		double fact_value = y[y.size() - 2];
+		double a;
 
 		int len = x1.size();
 
 		for (int i(0); i < len; i++) {
-
+			
+			a = predict_value;
 			predict_value = alpha * fact_value + (1 - alpha) * predict_value;
 			prediction.push_back(predict_value);
-			fact_value = predict_value;
+			fact_value = a;
 		}
 	}
-	//for (std::vector<double>::iterator r = y.begin(); r != y.end(); r++) {  // йюй асдрн дкъ с
-	//	fact_value = *r;
-	//	predict_value = alpha * fact_value + (1 - alpha) * predict_value;
-	//}
 
 	return prediction;
 }
