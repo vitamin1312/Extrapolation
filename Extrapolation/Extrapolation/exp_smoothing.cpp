93e3b19a729e6a026d9a2a406c3291a58d1cc56c
UstanovkaShkafov <112942211+UstanovkaShkafov@users.noreply.github.com>
2022-12-12 09:17:34 +0300

    Smooth(alpha checking)

--- Extrapolation/Extrapolation/exp_smoothing.cpp
+++ Extrapolation/Extrapolation/exp_smoothing.cpp
@@ -3,26 +3,22 @@
 #include <vector>
 
 std::vector<double> exponential_smoothing(std::vector<double> x, std::vector<double> y, std::vector<double> x1, double alpha) {
-	if (alpha > 1 || alpha < 0) {
-		std::vector <double> result = {};
-	/// если параметр alpha задан не корректно, возвращается ошибка
-	return result;
-	}
-
 	std::vector<double> prediction;
 
-	double predict_value = y[y.size() - 1];
-	double fact_value = y[y.size() - 2];
+	if (alpha > 0 && alpha < 1)
+	{
+		double predict_value = y[y.size() - 1];
+		double fact_value = y[y.size() - 2];
 
-	int len = x1.size();
+		int len = x1.size();
 
-	for (int i(0); i < len;i++) {
+		for (int i(0); i < len; i++) {
 
-		predict_value = alpha * fact_value + (1 - alpha) * predict_value;
-		prediction.push_back(predict_value);
-		fact_value = predict_value;
+			predict_value = alpha * fact_value + (1 - alpha) * predict_value;
+			prediction.push_back(predict_value);
+			fact_value = predict_value;
+		}
 	}
-
 	//for (std::vector<double>::iterator r = y.begin(); r != y.end(); r++) {  // КАК БУДТО ДЛЯ У
 	//	fact_value = *r;
 	//	predict_value = alpha * fact_value + (1 - alpha) * predict_value;
